3f896cee0d1bc671cab5b9ecdc3ad5735f0538f7
Bob <rbatzing@gmail.com>
2014-11-06 02:53:05 +0700

    Added menu

--- store.c
+++ store.c
@@ -28,15 +28,15 @@ int readProd()
    return(numProd);
 }
 
-int newProd()
+int addProd()
 {  Product x;
     FILE * fp;
    
-        printf("Scan barcode:");
-        scanf("%s", x.code);
-        printf("Enter price");
-        scanf("%f",&x.price);
-    fp =fopen("sample.dat","a");
+    printf("Scan barcode:");
+    scanf("%s", x.code);
+    printf("Enter price");
+    scanf("%f",&x.price);
+    fp = fopen("sample.dat","a");
     fprintf(fp,"%s %.2f\n", x.code,x.price );
 	fclose(fp);
 	return(1);
@@ -44,23 +44,57 @@ int newProd()
 
 int checkout()
 {
-
+   return(1);
 }
 
+int updateProd()
+{
+   return(1);
+}
+int deleteProd()
+{
+   return(1);
+}
 int main()
 {
 	int i;
+	char c;
 	
-	newProd();
-
 	numProd = readProd();
+	do 
+	{
+	   printf("Mini Store System Menu:\n");
+	   printf("\t[A]dd\n\t[D]elete\n\t[U]pdate\n");
+	   printf("\t[S]how\n\t[C]heckout\n\t[Q]uit\n");
+	   printf("Please Choose:");
+	   
+	   c = getchar();
+	   switch(c)
+	   {
+	   case 'A':
+	   case 'a':
+	   	addProd(); break;
+		
+	   case 'D':
+	     deleteProd(); break;
+	     
+	   case 'U':
+	      updateProd(); break;
+		  
+	   case 'C':
+		  checkout(); break;
+	   }
+    } while ((c != 'Q') && (c != 'q'));
 
-   
+    printf("Good-bye\n");
+
+  
    printf("Num of Products: %d\n", numProd);
    
 
    for(i = 0; i < numProd; i++)
-	    printf("Product: %2d Code:%13s Price: %8.2f\n", i,    p[i].code, p[i].price);
+	    printf("Product: %2d Code:%13s Price: %8.2f\n", i, p[i].code,
+			p[i].price);
 
    return(0);
 }
